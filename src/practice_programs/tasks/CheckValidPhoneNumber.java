307f2c606fec407faf1bf74082575207818ef73f
nikhil-biradar <nikhilbiradar878@gmail.com>
2025-04-12 16:18:21 +0530

    regex files updated

--- src/practice_programs/tasks/CheckValidPhoneNumber.java
+++ src/practice_programs/tasks/CheckValidPhoneNumber.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ChechValidPhoneNumber {
+public class CheckValidPhoneNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Phone Number : ");
